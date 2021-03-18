3d0123ad8b8908e3613ef8dbea521003710af19d
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-03-19 08:54:46 +0900

    Initial Commit

---
+++ Swift6Button1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Button1
+//
+//  Created by Kenta Yamada on 2021/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
